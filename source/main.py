e9e5f9024dec5c030f87431bd5684337c3ea610e
Matěj Kubíček <kubicekm23@spstrutnov.cz>
2024-09-16 15:43:07 +0200

    Základní struktura hry
    main.py - hlavní spouštěcí soubor
    menu.py - soubor obsahující vše týkající se menu
    classes.py - soubor ze kterého se dle potřeb budou načítat třídy
    
    Dle potřeb se budou přidávat další soubory, hlavně kvůli hře jako takové.

---
+++ source/main.py
@@ -0,0 +1,8 @@
+def main():
+    from menu import menu_main
+
+    menu_variables = menu_main()
+
+
+if __name__ == "__main__":
+    main()
---
+++ source/menu.py
@@ -0,0 +1,9 @@
+def menu_main():
+    import pygame
+    menu_window = pygame.display.set_mode((800, 600))
+    menu_open = True
+
+    while menu_open:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                menu_open = False
